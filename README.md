c9641cb076bc54f064323a4a92387cd826de75b3
vsnhatminh <vsnhatminh@gmail.com>
2023-10-24 14:47:51 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# RazorPageDemo
+ RazorPageDemo
---
+++ RazorPage/wwwroot/js/site.js
@@ -0,0 +1,8 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$('#btnS').click(function () {
+    alert('cong hoa xa hoi chu nghia viet nam');
+    });
